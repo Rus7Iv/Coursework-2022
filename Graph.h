048e0484d39f1e03279eb4392ffe8f632e3a6537
Rus7Iv <rusivary@gmail.com>
2022-06-16 17:05:14 +0400

    First commit

---
+++ Graph.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include <list> 
+using namespace std;
+
+// Класс Graph представляет собой ориентированный граф, использующий представление списка смежности
+class Graph
+{
+	int numberVertices;
+	list<int>* adjMatrix;// Указатель на массив, содержащий списки смежности
+public:
+	Graph();
+	Graph(int numberVertices);
+	bool addEdge(int numberVertices, int numberEdges);
+	void createGraph(Graph& graph, int M);
+	void connections(int w, bool visited[], int dis[], int low[], int par[]);
+	void findMaxCut();
+	void printGraph(); //вывод графа на экран
+};
